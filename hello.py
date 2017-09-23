83612ac8978dc8852160a374380deefefc69c3a6
Josh Bowen <mr.josh.bowen@gmail.com>
2017-09-23 13:40:21 -0500

    JSON db finished

--- hello.py
+++ hello.py
@@ -3,31 +3,52 @@ from flask import request
 import datetime, json
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return 'Welcome to Memoreyes</br>'+time
-
-@app.route('/api', methods=['GET','POST'])
+@app.route('/', methods=['POST'])
 def api():
-    if request.method == 'POST':
-        return parse_request()
-    if request.method == 'GET':
-        time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        if request.get_json() is True:
-            return json.dumps({'success':'true','time':time})
-        else:
-            return json.dumps({'success':'false','time':time})
-    else:
-        abort(401)
-        return 'ERROR'
+    time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    requestingUserId = request.form['userId']
+    f = request.files['photo']
+    f.save('uploaded_image.jpg')
+    return json.dumps({'success':'true','time':time})
 
-def parse_request():
-    return "Y U SEND POST"
+# returns a complete set of data for a given user
+def read_user_data(userId):
+    with open(userId+'.json', 'r') as dataFile:
+        data = json.load(dataFile)
+        return data
 
-# request.args.get('key', '')
+# writes a full set of data for a given user
+def write_user_data(userId, newData):
+    with open(userId+'.json', 'w') as dataFile:
+        json.dump(newData, dataFile)
+        return True
 
-# request.form['username']
+# adds a person to a user's data set
+def add_person(userId, personsName, personsRelationship):
+    data = read_user_data(userId)
+    data['people'][personsName] = personsRelationship
+    write_user_data(userId, data)
+    return True
 
-# f = request.files['the_file']
-# f.save('/var/www/uploads/uploaded_file.txt')
+# checks to see if a person is in a user's data set by name
+def check_for_person(userId, personsName):
+    data = read_user_data(userId)
+    if personsName in data['people']:
+        return True
+
+# adds a drug to a user's data set
+def add_drug(userId, drugName, drugInstructions):
+    data = read_user_data(userId)
+    data['drugs'][drugName] = drugInstructions
+    write_user_data(userId, data)
+    return True
+
+# checks to see if a drug is in a user's data set by name
+def check_for_drug(userId, drugName):
+    data = read_user_data(userId)
+    if drugName in data['drugs']:
+        return True
+
+# Debug DB
+    # dataObject = read_user_data('123456')
+    # print(json.dumps(dataObject, indent=2, separators=(',', ': ')))
